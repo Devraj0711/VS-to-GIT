855dab32458a5c1e69eacf8f73189da9c29660aa
Devraj0711 <singhdevrajtomar7>
2023-08-17 14:33:20 +0530

    message stored in the file displayed on the screen

--- NodeJS_/StoringFiles.js
+++ NodeJS_/StoringFiles.js
@@ -1,41 +1,58 @@
 const http = require("http");
+const fs = require("fs");
 
-const server= http.createServer((req, res)=>
-{
+const server = http.createServer((req, res) => {
     const url = req.url;
-    const method= req.method;
-    if(url==='/')
-    {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title><head>');
-        res.write('<body><form action= "/message" method="POST"><input type="text" name="message"> <button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    const fs=require('fs');//importing FS module
-    if(url === '/message' && method ==='POST')
-    {
-        const body=[];
-        req.on('data', (chunk)=>{
-            console.log(chunk);
+    const method = req.method;
+
+    if (url === "/") {
+        res.write("<html>");
+        res.write("<head><title>Enter Message</title></head>");
+        
+        // Read messages from file and display at the top of the form
+        fs.readFile("message.txt", "utf8", (err, data) => {
+            if (!err) {
+                const messages = data.split("\n").filter(message => message.trim() !== "");
+                res.write("<ul>");
+                messages.forEach(message => {
+                    res.write(`<li>${message}</li>`);
+                });
+                res.write("</ul>");
+            }
+            res.write("<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form>");         
+            res.write("</body>");
+            res.write("</html>");
+            res.end();
+        });
+    } else if (url === "/message" && method === "POST") {
+        const body = [];
+        req.on("data", chunk => {
             body.push(chunk);
         });
-        req.on('end', ()=>{
-            const parsedBody= Buffer.concat(body).toString();
-            const message= parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-        
-        res.statusCode=302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title><head>');
-    res.write('<body><h1>Hello</h1></body>');
-    res.write('</html>');
-    res.end();    
 
+        req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+           
+            // Append the new message to the file
+            fs.appendFile("message.txt", message + "\n", err => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+
+            res.statusCode = 302;
+            res.setHeader("Location", "/");
+            return res.end();
+        });
+    } else {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<html>");
+        res.write("<head><title>My First Page</title></head>");
+        res.write("<body><h1>Hello</h1></body>");
+        res.write("</html>");
+        res.end();
+    }
 });
+
 server.listen(3000);
