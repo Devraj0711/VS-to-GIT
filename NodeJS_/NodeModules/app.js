4cdfc379b6b27f3ad0b75cc9667d0e6e79ed985a
Devraj0711 <69614639+Devraj0711@users.noreply.github.com>
2023-08-21 22:10:57 +0530

    Add files via upload

--- NodeJS_/NodeModules/app.js
+++ NodeJS_/NodeModules/app.js
@@ -1,6 +1,25 @@
 const http= require('http');
-const routes= require('./routes');
-console.log(routes.someText);
-const server=http.createServer(routes.handler);
 
+const express= require('express');
+
+const app= express();
+
+app.use((req,res, next)=>{
+    console.log('In the middleware!');
+    next();  // allows the request to continue to the next middlewear in line
+});
+
+app.use((req,res, next)=>{
+    console.log('In 2nd middleware!');
+    res.send('<h1>Hello from Express!</h1>')// ...
+});
+
+//const routes= require('./routes');
+//console.log(routes.someText);
+
+//instead of below code:-
+const server=http.createServer(app);
 server.listen(3000);
+
+//we can use 
+app.listen(3000);
